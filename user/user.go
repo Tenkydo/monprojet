490ccbe694116b2c59263f6f56c84c4955fdc1f6
Tenkydo <baudet513@gmail.com>
2025-09-02 12:30:08 +0000

    update user: ajout getters et setters pour age/email

--- user/user.go
+++ user/user.go
@@ -1,16 +1,22 @@
 package user
 
+import "fmt"
+
 // User représente un utilisateur
 type User struct {
-	Name  string
-	Login string
+	Name  string // public
+	Login string // public
+	email string // privé
+	age   int    // privé
 }
 
 // New crée un nouvel utilisateur et retourne un pointeur vers User
-func New(name, login string) *User {
+func New(name, login, email string, age int) *User {
 	return &User{
 		Name:  name,
 		Login: login,
+		email: email,
+		age:   age,
 	}
 }
 
@@ -19,7 +25,25 @@ func (u *User) UpdateName(name string) {
 	u.Name = name
 }
 
-// GetName retourne le nom et le login sous forme de chaîne
-func (u *User) GetName() string {
-	return u.Name + " -> " + u.Login
+// --- GESTION EMAIL ---
+func (u *User) GetEmail() string {
+	return u.email
+}
+
+func (u *User) UpdateEmail(email string) {
+	u.email = email
+}
+
+// --- GESTION AGE ---
+func (u *User) GetAge() int {
+	return u.age
+}
+
+func (u *User) UpdateAge(age int) {
+	u.age = age
+}
+
+// Retourne toutes les infos
+func (u *User) GetInfo() string {
+	return fmt.Sprintf("%s (%s), %s, âge: %d", u.Name, u.Login, u.email, u.age)
 }
